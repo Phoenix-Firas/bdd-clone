b1476d17681e465fab001c0b256873ee51b73fca
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-09-03 11:14:18 +0300

    finished explore-section

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ var w = window.innerWidth;
 var header = document.getElementById('header');
 var header_links = document.getElementById('header-links');
 
+var explore_img = document.getElementById('explore-img');
 
     var nav_btn = document.createElement('div');
     nav_btn.classList.add('nav-btn')
-
     header.append(nav_btn);
-
     var nav_hidden = true;
 
     nav_btn.addEventListener('click', ()=>{
-
         if(nav_hidden){
             header_links.style.animation = 'show-nav 1s ease-out forwards';
             nav_btn.style.animation = 'collapse-nav-btn .5s ease forwards';
@@ -21,4 +19,7 @@ var header_links = document.getElementById('header-links');
             nav_btn.style.animation = 'grow-nav-btn .5s ease forwards';
             nav_hidden = true;
         }
+
     })
+
+
